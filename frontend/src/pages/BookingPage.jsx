8a4bc2b84430d3edb91bd79ccfd87540c281aa3c
kaveritota <157624361+kaveritota@users.noreply.github.com>
2025-07-17 17:00:51 +0530

    changes occured in booking form

--- frontend/src/pages/BookingPage.jsx
+++ frontend/src/pages/BookingPage.jsx
@@ -17,7 +17,7 @@ const BookingPage = () => {
         setRestaurant(data);
       } catch (err) {
         console.error(err);
-        navigate('/home');
+        navigate('/restaurants');
       } finally {
         setLoading(false);
       }
